a57d750cfeff56672be21f35c241eca10c2e74fa
marlonsc-dc <marlon.costa@datacosmos.com.br>
2024-09-29 20:56:11 -0300

    Correct port conversion int

--- internal/helpers/backend/generic.go
+++ internal/helpers/backend/generic.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"github.com/db-operator/db-operator/api/v1beta1"
@@ -9,6 +10,7 @@ import (
 	"github.com/db-operator/db-operator/pkg/utils/database"
 	"github.com/db-operator/db-operator/pkg/utils/dbinstance"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 type GenericBackend struct {
@@ -25,6 +27,7 @@ func (g *GenericBackend) GetInstance(ctx context.Context, dbin *v1beta1.DbInstan
 	var port uint16
 	var publicIP string
 	var err error
+	log := log.FromContext(ctx)
 
 	if from := dbin.Spec.Generic.HostFrom; from != nil {
 		host, err = g.kubeHelper.GetValueFrom(ctx, from.Kind, from.Namespace, from.Name, from.Key)
@@ -42,6 +45,11 @@ func (g *GenericBackend) GetInstance(ctx context.Context, dbin *v1beta1.DbInstan
 		}
 		port64, err := strconv.ParseUint(portStr, 10, 64)
 		if err != nil {
+			return nil, err
+		}
+		if port64 < 0 || port64 > 65535 {
+			err := fmt.Errorf("port value out of range: %d", port64)
+			log.Error(err, "port value is out of the valid range (0-65535)")
 			return nil, err
 		}
 		port = uint16(port64)
