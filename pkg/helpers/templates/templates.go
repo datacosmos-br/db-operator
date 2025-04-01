31fe746573f6d07b2dc2aec8de86c3a1fca28e2c
marlon-costa-dc <marlon.costa@datacosmos.com.br>
2025-03-31 22:56:46 -0300

    local changes

--- pkg/helpers/templates/templates.go
+++ pkg/helpers/templates/templates.go
@@ -27,7 +27,7 @@ import (
 	"github.com/db-operator/db-operator/api/v1beta1"
 	"github.com/db-operator/db-operator/pkg/consts"
 	"github.com/db-operator/db-operator/pkg/types"
-	"github.com/db-operator/db-operator/pkg/helpers/database"
+	"github.com/db-operator/db-operator/pkg/utils/database"
 	corev1 "k8s.io/api/core/v1"
 
 	"k8s.io/utils/strings/slices"
